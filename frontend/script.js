a168cf67ec048ad5e18c414b7d6185790355332c
Monica Leiva <lau23leiva@gmail.com>
2026-03-09 13:28:20 -0300

    Publish books form

--- frontend/script.js
+++ frontend/script.js
@@ -9,10 +9,12 @@ books.forEach(book => {
     bookCard.classList.add("book");
 
     bookCard.innerHTML = `
-        <h3>${book.title}</h3>
-        <p>Autor: ${book.author}</p>
-        <p>Precio: $${book.price}</p>
-        <p>Estado: ${book.condition}</p>
+        <h3>${book.titulo}</h3>
+        <p>Autor: ${book.autor}</p>
+        <p>Género: ${book.genero}</p>
+        <p>${book.descripcion}</p>
+        <p>Precio: $${book.precio}</p>
+        <p>${book.intercambio ? "Disponible para intercambio" : ""}</p>
     `;
 
     bookList.appendChild(bookCard);
@@ -25,18 +27,45 @@ guardarBtn.addEventListener("click", function() {
   const autor = document.getElementById("autor").value
   const genero = document.getElementById("genero").value
   const descripcion = document.getElementById("descripcion").value
+  const precio = document.getElementById("precio").value
+  const intercambio = document.getElementById("intercambio").checked
+  
 
   const nuevoLibro = {
     titulo: titulo,
     autor: autor,
     genero: genero,
-    descripcion: descripcion
+    descripcion: descripcion,
+    precio: precio,
+    intercambio: intercambio
+
   }
 
   books.push(nuevoLibro)
 
   localStorage.setItem("books", JSON.stringify(books))
 
+  const bookCard = document.createElement("div");
+  bookCard.classList.add("book");
+
+  bookCard.innerHTML = `
+  <h3>${titulo}</h3>
+  <p>Autor: ${autor}</p>
+  <p>Género: ${genero}</p>
+  <p>${descripcion}</p>
+  <p>Precio: $${precio}</p>
+  <p>${intercambio ? "Disponible para intercambio" : ""}</p>
+  `;
+
+  bookList.appendChild(bookCard);
+
+  document.getElementById("titulo").value = ""
+  document.getElementById("autor").value = ""
+  document.getElementById("genero").value = ""
+  document.getElementById("descripcion").value = ""
+  document.getElementById("precio").value = ""
+  document.getElementById("intercambio").checked = false
+
+
+
 });
-
-
